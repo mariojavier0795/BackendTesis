d2f1b7dcb0c77e57be196430fc60ede382dd590d
Mario Giler <mario.giler@fit-bank.com>
2020-05-10 16:10:42 -0500

    Servicios kotlin

---
+++ backendtesis/src/main/kotlin/com/example/tesis/backend/BackendApplication.kt
@@ -0,0 +1,29 @@
+package com.example.tesis.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendApplication>(*args)
+}
+
+@RequestMapping("/servicios")
+@RestController
+class RestController {
+    @GetMapping("/prueba")
+    fun example() : ResponseEntity<String> {
+        return ResponseEntity<String>("Hello World!", HttpStatus.OK)
+    }
+
+}
+
+data class Cliente (val nombre : String, val apellido : String)
