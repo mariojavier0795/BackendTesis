92aa9384ea666be587d554f4b2e4f4f3868c5327
Mario Giler <mario.giler@fit-bank.com>
2020-05-26 12:32:39 -0500

    Nuevos servicios

--- backendtesis/src/main/kotlin/com/example/tesis/backend/controller/PersonController.kt
+++ backendtesis/src/main/kotlin/com/example/tesis/backend/controller/PersonController.kt
@@ -2,18 +2,21 @@ package com.example.tesis.backend.controller
 
 import com.example.tesis.backend.repository.PersonRepository
 import com.example.tesis.backend.entity.Persona
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @CrossOrigin("*")
 @RequestMapping("/person")
 @RestController
 class PersonController(private val personRepository : PersonRepository) {
+
     @GetMapping("/searchPerson")
     fun getSearchPerson() : List<Persona> {
-        return personRepository.findAll();
+        return personRepository.findAll()
+    }
+
+    @PostMapping("/searchNamesPerson")
+    fun searchNamesPerson(@RequestBody persona: Persona) : List<Persona> {
+        return personRepository.findByNameOrLastname(persona.nombres, persona.apellidos)
     }
 }
