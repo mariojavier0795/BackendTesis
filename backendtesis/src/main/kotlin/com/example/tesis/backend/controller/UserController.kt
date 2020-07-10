8ca76b593c029c97d8043161ce1b743b4b95aa4c
Mario Giler <mario.giler@fit-bank.com>
2020-07-09 22:21:08 -0500

    Backend

--- backendtesis/src/main/kotlin/com/example/tesis/backend/controller/UserController.kt
+++ backendtesis/src/main/kotlin/com/example/tesis/backend/controller/UserController.kt
@@ -1,15 +1,8 @@
 package com.example.tesis.backend.controller
 
 
-import com.example.tesis.backend.entity.Role
 import com.example.tesis.backend.entity.User
-import com.example.tesis.backend.entity.UserRoles
-import com.example.tesis.backend.repository.UserRepository
-import com.example.tesis.backend.repository.UserRolesRepository
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.node.ObjectNode
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
+import com.example.tesis.backend.service.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -17,30 +10,16 @@ import org.springframework.web.bind.annotation.*
 @CrossOrigin("*")
 @RequestMapping("/user")
 @RestController
-class UserController(private val userRepository : UserRepository) {
-
-    val jsonMap: HashMap<String, Any> = HashMap()
-
-    @GetMapping("/loginUser")
-    fun loginUser(@RequestParam() parameters: Map<String, String>) : ResponseEntity<Any> {
-        return try {
-            val listResponseUser = userRepository.findUser(parameters.get("username"), parameters.get("password"))
-            jsonMap["Array"] = listResponseUser
-            jsonMap["Status"] = "True"
-            if (listResponseUser.isEmpty()) {
-                jsonMap["Message"] = "Usuario y/o contraseña están incorrectos"
-                jsonMap["Status"] = "False"
-            }
-            ResponseEntity(jsonMap, HttpStatus.OK)
-        } catch (e: Exception) {
-            jsonMap["Error message"] = e.message.toString()
-            ResponseEntity(jsonMap, HttpStatus.BAD_REQUEST)
-        }
-    }
+class UserController(private val userService: UserService) {
 
     @PostMapping("/saveUser")
-    fun saveUser(@RequestBody() user: User): User {
-        return userRepository.save(user)
+    fun saveUser(@RequestBody user: User): ResponseEntity<User> = userService.saveUser(user)
+
+    @GetMapping("/logInUser")
+    fun logInUser(@RequestParam parameters: Map<String, String>): ResponseEntity<List<User>> {
+        val username = parameters["username"]
+        val password = parameters["password"]
+        return userService.logInUser(username, password)
     }
 
 }
---
+++ backendtesis/src/main/kotlin/com/example/tesis/backend/service/UserService.kt
@@ -0,0 +1,43 @@
+package com.example.tesis.backend.service
+
+import com.example.tesis.backend.entity.User
+import com.example.tesis.backend.repository.UserRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+
+@Service()
+class UserService(private val userRepository : UserRepository) {
+
+    fun saveUser(user: User): ResponseEntity<User> =
+            ResponseEntity.ok(userRepository.save(user))
+
+    fun logInUser(username: String?, password: String?): ResponseEntity<List<User>> =
+            ResponseEntity.ok(userRepository.findUser(username, password))
+
+    /*fun getTaskById(taskId: Long): ResponseEntity<Task> =
+            taskRepository.findById(taskId).map { task ->
+                ResponseEntity.ok(task)
+            }.orElse(ResponseEntity.notFound().build())
+
+    fun putTask(taskId: Long, newTask: Task): ResponseEntity<Task> =
+            taskRepository.findById(taskId).map { currentTask ->
+                val updatedTask: Task =
+                        currentTask
+                                .copy(
+                                        title = newTask.title,
+                                        description = newTask.description,
+                                        status = newTask.status,
+                                        startDate = newTask.startDate,
+                                        priority = newTask.priority,
+                                        dueDate = newTask.dueDate
+                                )
+                ResponseEntity.ok().body(taskRepository.save(updatedTask))
+            }.orElse(ResponseEntity.notFound().build())
+
+    fun deleteTask(taskId: Long): ResponseEntity<Void> =
+            taskRepository.findById(taskId).map { task ->
+                taskRepository.delete(task)
+                ResponseEntity<Void>(HttpStatus.ACCEPTED)
+            }.orElse(ResponseEntity.notFound().build())*/
+
+}
